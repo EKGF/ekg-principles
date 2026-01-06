ecc73eedfcc1a7c2499e5f6e17b3c156a61cf9df
Jacobus Geluk <jacobus.geluk@ekgf.org>
2026-01-06 00:42:22 +0000

    fix(js): correct palette switcher IDs from __palette_1/2 to __palette_0/1

--- docs/javascript/images_dark.js
+++ docs/javascript/images_dark.js
@@ -1,15 +1,15 @@
 (function() {
+const paletteSwitcher0 = document.getElementById("__palette_0");
 const paletteSwitcher1 = document.getElementById("__palette_1");
-const paletteSwitcher2 = document.getElementById("__palette_2");
 
-if (paletteSwitcher1) {
-    paletteSwitcher1.addEventListener("change", function () {
+if (paletteSwitcher0) {
+    paletteSwitcher0.addEventListener("change", function () {
         documentFromDarkToLight(document)
     });
 }
 
-if (paletteSwitcher2) {
-    paletteSwitcher2.addEventListener("change", function () {
+if (paletteSwitcher1) {
+    paletteSwitcher1.addEventListener("change", function () {
         documentFromLightToDark(document)
     });
 }
